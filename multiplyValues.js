6ad247431322b3ddf7557bb214baba511c290d7f
Rick Clark <rkingsleyclark@gmail.com>
2018-05-08 13:24:21 +0100

    Add multiplyValues fn

---
+++ multiplyValues.js
@@ -0,0 +1,7 @@
+var a = 12;
+var b = 3;
+function multiplyValues(firstValue, secondValue) {
+    var product = firstValue * secondValue;
+    console.log('product is ', product);
+}
+multiplyValues(a, b);
---
+++ multiplyValues.ts
@@ -0,0 +1,9 @@
+const a: number = 12;
+const b: number = 3;
+
+function multiplyValues(firstValue: number, secondValue: number): void {
+  const product = firstValue * secondValue;
+  console.log('product is ', product);
+}
+
+multiplyValues(a,b);
